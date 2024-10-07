264b0967413b593e4778a2958181c5e74dc9b781
NikiValle <valle.nicola.studente@itispaleocapa.it>
2024-10-07 13:36:14 +0200

    Metodo TrovaGiocatore

--- src/Pack/TestGiocatore.java
+++ src/Pack/TestGiocatore.java
@@ -3,9 +3,12 @@ import java.util.Scanner;
 public class TestGiocatore {
     public static void main(String[]args){
         Scanner in = new Scanner(System.in);
-        int scelta;
+        int sceltaNumerica;
+        String scelta;
         boolean active=true;
+        int giocatoreInModifica;
         int nGiocatori =0;
+        String modGiocatore;
         String nome;
         String capitano;
         boolean presenzaCapitano = false;
@@ -22,8 +25,8 @@ public class TestGiocatore {
                     "5.Visualizza i giocatori che hanno realizzato più di 5 goal.\n" +
                     "6.Visualizza il nome del capitano\n" +
                     "7.Assegna il ruolo di capitano in modo casuale se non ancora presente");
-            scelta=in.nextInt();
-            switch (scelta){
+            sceltaNumerica=in.nextInt();
+            switch (sceltaNumerica){
                 case 1:
                     System.out.println("Inserisci i dati del nuovo giocatore in quest'ordine: Nome, capitano(si/no), numero di gol");
                     nome=in.next();
@@ -49,6 +52,31 @@ public class TestGiocatore {
                     }
                     break;
                 case 3:
+                    System.out.println("Inserisci nome del giocatore di cui vuoi modificare i dati");
+                    modGiocatore=in.next();
+                    giocatoreInModifica=TrovaGiocatore(g,nGiocatori, modGiocatore);
+                    if(giocatoreInModifica==-1){
+                        System.out.println("Nessun giocatore trovato con questo nome");
+                        break;
+                    }
+                    System.out.println("Scegli il dato che vuoi modificare: Nome, capitano, numero gol");
+                    scelta=in.next();
+                    if(scelta.equalsIgnoreCase("nome")){
+                        System.out.println("Inserisci il nuovo nome");
+                        g[giocatoreInModifica].setNome(in.next());
+                    }
+                    else if(scelta.equalsIgnoreCase("capitano")){
+                        System.out.println("Valore capitano invertito");
+                        if(g[giocatoreInModifica].getCapitano()==false)
+                            g[giocatoreInModifica].setCapitano(true);
+                        else
+                            g[giocatoreInModifica].setCapitano(false);
+                    }
+                    else{
+                        System.out.println("Inserisci il nuovo numero di gol");
+                        g[giocatoreInModifica].setGol(in.nextInt());
+                    }
+                    break;
 
             }
         }while(active);
@@ -64,5 +92,13 @@ public class TestGiocatore {
             c="non è capitano";
         String output = (g[i].getNome()+", "+c+", "+g[i].getGol());
         return output;
+    }
+    public static int TrovaGiocatore(Giocatore[]g, int nGiocatori, String nome){
+        for(int i=0;i<nGiocatori+1;i++){
+            if(g[i].getNome().equalsIgnoreCase(nome))
+                return i;
+            break;
+        }
+        return -1;
     }
 }
