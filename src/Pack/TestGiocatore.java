7daa249e8686d9c79c56aa319dda8f1621a01857
NikiValle <valle.nicola.studente@itispaleocapa.it>
2024-10-07 13:14:36 +0200

    Metodo VisualizzaGiocatori

--- src/Pack/TestGiocatore.java
+++ src/Pack/TestGiocatore.java
@@ -5,8 +5,9 @@ public class TestGiocatore {
         Scanner in = new Scanner(System.in);
         int scelta;
         boolean active=true;
-        int nGiocatori;
+        int nGiocatori =0;
         String nome;
+        String capitano;
         boolean isCapitano;
         int gol;
         Giocatore[] g = new Giocatore[1000];
@@ -23,11 +24,35 @@ public class TestGiocatore {
             switch (scelta){
                 case 1:
                     System.out.println("Inserisci i dati del nuovo giocatore in quest'ordine: Nome, capitano(si/no), numero di gol");
+                    nome=in.next();
+                    capitano=in.next();
+                    if(capitano.equalsIgnoreCase("no"))
+                        isCapitano=false;
+                    else
+                        isCapitano=true;
+                    gol=in.nextInt();
+                    CreaGiocatore(nome, isCapitano, gol, g, nGiocatori);
+                    nGiocatori++;
+                    break;
+                case 2:
+                    for(int i=0;i<nGiocatori+1;i++){
+                        System.out.println(VisualizzaGiocatori(g, i));
+                    }
+                    break;
 
             }
         }while(active);
     }
     public static void CreaGiocatore(String Nome, boolean Capitano, int Gol, Giocatore[] g, int nGiocatori){
         g[nGiocatori] = new Giocatore(Nome, Capitano, Gol);
+    }
+    public static String VisualizzaGiocatori(Giocatore[] g, int i){
+        String c;
+        if(g[i].getCapitano())
+            c="è capitano";
+        else
+            c="non è capitano";
+        String output = (g[i].getNome()+", "+c+", "+g[i].getGol());
+        return output;
     }
 }
